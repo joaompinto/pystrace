6c188b25b8d5ecacd133879927232ae29084fea7
Andre <maroneze@users.noreply.github.com>
2026-09-16 14:33:44 +0200

    Add --output/-o option for file output

--- pystrace/cli.py
+++ pystrace/cli.py
@@ -28,6 +28,14 @@ def parse_cmd_line():
         dest="timeout",
         default=None,
     )
+    parser.add_option(
+        "--output",
+        "-o",
+        help="write trace output to FILE (default: stderr)",
+        dest="output",
+        metavar="FILE",
+        default=sys.stderr,
+    )
     options, args = parser.parse_args()
     if len(args) == 0:
         print("Usage: {} [options] command".format(sys.argv[0]))
